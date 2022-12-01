92ce8c7cb28170ba136d32b5f355c12b46a03105
Gyula Szabo <44570623+gyuvi02@users.noreply.github.com>
2022-12-01 11:28:01 +0100

    CSS set and search completed

--- src/main/java/org/gyula/javaee_shop/servlets/SearchServlet.java
+++ src/main/java/org/gyula/javaee_shop/servlets/SearchServlet.java
@@ -5,8 +5,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.gyula.javaee_shop.beans.Product;
+import org.gyula.javaee_shop.dao.ApplicationDao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.List;
 
 
 @WebServlet("/search")
@@ -17,15 +23,16 @@ public class SearchServlet extends HttpServlet {
 	
 		//collect search string from the form
 		String searchString = req.getParameter("search");
+		ApplicationDao dao = new ApplicationDao();
+		List<Product> products = dao.searchProducts(searchString);
 		
 		
 		//call DAO layer and get all products for search criteria
-		
-		
+
+
 		//write the products data back to the client browser
-		
-		
-		
+		String page = getHTMLString(req.getServletContext().getRealPath("src/main/webapp/searchResults.html"), products);
+		resp.getWriter().write(page);
 	}
 	
 	/**
@@ -36,6 +43,24 @@ public class SearchServlet extends HttpServlet {
 	 * @return
 	 * @throws IOException
 	 */
-	
-	
+	public String getHTMLString(String filePath, List<Product> products) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		String line="";
+		StringBuffer buffer = new StringBuffer();
+		while((line=reader.readLine())!=null){
+			buffer.append(line);
+		}
+
+		reader.close();
+		String page = buffer.toString();
+
+		page = MessageFormat.format(page, products.get(0).getProductImgPath(),
+				products.get(1).getProductImgPath(),products.get(2).getProductImgPath(),
+				products.get(0).getProductName(),products.get(1).getProductName(),
+				products.get(2).getProductName(),0);
+
+		return page;
+	}
+
+
 }
